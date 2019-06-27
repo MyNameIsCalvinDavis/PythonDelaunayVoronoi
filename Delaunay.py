a00d25affec01f8f209af3978f12298443ed64ef
Calvin Davis <mynameiscalvindavis@gmail.com>
2019-06-27 16:44:28 -0600

    Make it work
    Theres still a lot of unneccesary code here but it works

--- Delaunay.py
+++ Delaunay.py
@@ -4,44 +4,38 @@ import random
 import PIL
 import PIL.Image as Image
 from PIL import ImageTk
-from graphics import *
+import graphics as g
 import time
-
-"""
-
-How does it work?
-
-- First, the program plots 1000 points randomly on a grid.
-
-- Second, using the Delaunay Triangulation, the program triangulates every point.
-    - It creates an object (named tri) that acts as a grid
-    - In this, you can iterate through all of the triangles (simplices)
-      and get the color of the image at the center of that triangle.
-        - To break it down:
-        > Iterate through every triangle in the grid
-        > Get the center of that triangle
-        > Find the color of the pixel on the image at the center of the triangle
-        
-- Lastly, it iterates through every triangle and graphs the vertices of 
-  that triangle to the grid, with the color at the circumcenter.
-"""
+import PIL.ImageDraw as ImageDraw
 
 lastimg = [5, "P-2.png"] # Used so the same image isnt used twice. 5 is a placeholder.
 
 while True:# The main loop, this will run forever until stopped manually.
-    win = GraphWin("Delaunay Triangulation - Calvin Davis", 1200,1000) # The grid
-    choices = ["I-" + str(x) + ".png" for x in range(0, 20)] # The pictures
-    img = random.choice(choices)
+    win = g.GraphWin("Delaunay Triangulation", 1200,1000) # The grid
+    #choices = ["I-" + str(x) + ".png" for x in range(0, 20)] # The pictures
+    
+    """
+    Python 2.7's tkinter doesn't like dealing with png files
+    so instead of downloading a bunch of single frame gif files
+    i've decided instead to do nothing
+    """
+    
+    #img = random.choice(choices)
     img = "I-9.gif"
     
+    """
     if img in lastimg:
         continue
     lastimg[0] = img
+    """
     
-    pixelsg = Image(Point(0,0), img)
+    saveimg = Image.new("RGB", (1000,1000), "white")
+    draw = ImageDraw.Draw(saveimg)
+    
+    pixelsg = g.Image(g.Point(0,0), img)
     copy = pixelsg
     width, height = copy.getWidth(), copy.getHeight() # Gets the width and height of the image
-    pixelsg = Image(Point(width/2, height/2), img)
+    pixelsg = g.Image(g.Point(width/2, height/2), img)
     #pixelsg.draw(win)
     time.sleep(1)
     
@@ -72,23 +66,25 @@ while True:# The main loop, this will run forever until stopped manually.
         poly = []
         for vertex in simplex: # Keep in mind vertex is an X, Y pair
             poly.append([vertex[0], vertex[1]])
+        polycopy = poly[::]
         # Now we have the polygon
         ax = (poly[0][0] + poly[1][0] + poly[2][0]) * (1.0/3)
         ay = (poly[0][1] + poly[1][1] + poly[2][1]) * (1.0/3) # The center of the triangle
         rgb = pixelsg.getPixel(int(ax), int(ay)) # The color at the center of the triangle
         for item in range(len(poly)):# Turning the vertices into points that the graph can understand
-            poly[item] = Point(poly[item][0], poly[item][1])
-        triangle = Polygon(poly) # Plotting the triangle
-        triangle.setFill(color_rgb(rgb[0], rgb[1], rgb[2]))
-        triangle.setOutline(color_rgb(rgb[0], rgb[1], rgb[2]))
+            poly[item] = g.Point(poly[item][0], poly[item][1])
+        triangle = g.Polygon(poly) # Plotting the triangle
+        triangle.setFill(g.color_rgb(rgb[0], rgb[1], rgb[2]))
+        triangle.setOutline(g.color_rgb(rgb[0], rgb[1], rgb[2]))
+        
+        # I actually stole this line from another version of this code that is much more condensed
+        # Because i used to like to try to fit code into as few lines as possible
+        # As a result I have no idea what it actually does, I just know it saves the image
+        draw.polygon([tuple(x) for x in polycopy], fill=g.color_rgb(pixelsg.getPixel(int(ax), int(ay))[0], pixelsg.getPixel(int(ax), int(ay))[1], pixelsg.getPixel(int(ax), int(ay))[2]))
         triangle.draw(win)
-
-    """
-    plt.triplot(p[:,0], p[:,1], tri.vertices)
-    plt.plot(p[:,0], p[:,1], "o")
-    plt.show()
-    """
     print "Done!"
-    time.sleep(2)
+    saveimg.save("Output.bmp")
+    time.sleep(3)
     win.close()
+    
     break
