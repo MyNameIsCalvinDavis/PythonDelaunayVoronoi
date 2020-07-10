8e1809624fdf0a09a15618a8ecab9cc0f4811db8
MyNameIsCalvinDavis <mynameiscalvindavis@gmail.com>
2020-07-09 22:58:24 -0600

    Successfully created Voronoi Data structure, now I need to throw it all in a class or module

--- DV.py
+++ DV.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+import math
 
 class Edge:
 	def __init__(self, p1=(0,0), p2=(0,0) ):
@@ -10,8 +11,70 @@ class Edge:
 			return True
 		return False
 
+class VoronoiCell:
+	def __init__(self, triangles):
+
+		self.triangles = triangles
+		self.node = self.triangles.pop()
+		self.DelauynayPointList = self._calculatePointsFromTriangles(triangles)
+		self.VoronoiPointList = [t.cc for t in triangles]
+		print("OGPL", self.VoronoiPointList)
+
+		self.edges = self._calculateEdges()
+		self.P0 = 0
+
+	def _calculatePointsFromTriangles(self, triangles):
+		l = []
+		for t in triangles:
+			for point in t.points:
+				if not point in l:
+					l.append(point)
+		if self.node in l:
+			l.remove(self.node)
+
+		return l
+
+	def _calculateEdges(self):
+		"""
+		The cell has vertices associated with it, but not edges, and the vertices aren't
+		sorted so it's not as easy as just connecting them. To make the cell walls, we'll
+		be using the Graham Scan algorithm which procudes a convex hull given a set of points.
+		"""
+
+		f = self._grahamScan()
+		for i in range(len(f) - 1):
+			self.edges.append(Edge(f[i], f[i+1]))
+		self.edges.append(f[-1], f[0])
+
+	def _grahamScan(self):
+		# Lowest y coordinate and leftmost point, in that priority order
+		stack = []
+		P0 = min(self.VoronoiPointList, key=lambda x: x[1])
+		p = sorted( self.VoronoiPointList, key = lambda x: self._polarAngle(P0, x) )
+
+		# TODO: Check if points have the same polar angle
+		# Kinda hoping this problem just disappears but i know it wont
+
+		for point in p:
+			while len(stack) > 1 and self._orientation(stack[-2], stack[-1], point) < 0:
+				stack.pop()
+			stack.append(point)
+		return stack
+
+
+	def _orientation(self, p1, p2, p3):
+		v = (p2[1] - p1[1]) * (p3[0] - p2[0]) - (p2[0] - p1[0]) * (p3[1] - p2[1])
+		if v == 0: return 0
+		if v > 0: return 1
+		else: return 2
+
+	def _polarAngle(self, A, B): # param: (x, y)
+		B = (B[0] - A[0], B[1] - A[1])
+		return math.degrees(math.atan((B[1] / (B[0] + 0.001))))
+
+
 class Triangle:
-	def __init__(self, p1=(0,0), p2=(0,0), p3=(0,0) ):
+	def __init__(self, p1, p2, p3 ):
 		self.p1 = p1
 		self.p2 = p2
 		self.p3 = p3
@@ -20,13 +83,12 @@ class Triangle:
 		self.ccr = 0
 		self.edges = [ Edge(p1, p2), Edge(p2, p3), Edge(p3, p1) ]
 		
-		self.__calculatecc()
+		self._calculatecc()
 	
 	def __str__(self):
 		return str((self.p1, self.p2, self.p3))
 	
-	def __calculatecc(self): # Calculate circumcenter
-		# Sanitize
+	def _calculatecc(self): # Calculate circumcenter
 		if ((self.p1[0] == self.p2[0] == self.p3[0]) or (self.p1[1] == self.p2[1] == self.p3[1])):
 			raise Exception("Can not calculate circumcenter for points on the same line")
 			
@@ -34,7 +96,6 @@ class Triangle:
 		D = 2*( self.p1[0]*(self.p2[1] - self.p3[1])  +  self.p2[0]*(self.p3[1] - self.p1[1])  +  self.p3[0]*(self.p1[1] - self.p2[1]) )
 		self.cc[0] = (1 / D) * ( (self.p1[0]**2 + self.p1[1]**2)*(self.p2[1] - self.p3[1]) + (self.p2[0]**2 + self.p2[1]**2)*(self.p3[1] - self.p1[1]) + (self.p3[0]**2 + self.p3[1]**2)*(self.p1[1] - self.p2[1]) )
 		self.cc[1] = (1 / D) * ( (self.p1[0]**2 + self.p1[1]**2)*(self.p3[0] - self.p2[0]) + (self.p2[0]**2 + self.p2[1]**2)*(self.p1[0] - self.p3[0]) + (self.p3[0]**2 + self.p3[1]**2)*(self.p2[0] - self.p1[0]) )
-		
 		self.ccr = ( (self.p1[0] - self.cc[0])**2 + (self.p1[1] - self.cc[1])**2 )**0.5
 
 def findNeighbors(tri, tris):
@@ -77,10 +138,10 @@ def Triangulate(pointList):
 	Implementation of the Bowyer-Watson triangulation
 	"""
 
-	BL = ( # Find the bottom left most point
-		min(pointList, key = lambda x: x[0])[0] + 1,
-		min(pointList, key = lambda x: x[1])[1] + 1
-	)
+	# BL = ( # Find the bottom left most point
+	# 	min(pointList, key = lambda x: x[0])[0] + 1,
+	# 	min(pointList, key = lambda x: x[1])[1] + 1
+	# )
 	TR = ( # Find the top right most point
 		max(pointList, key=lambda x: x[0])[0] + 1,
 		max(pointList, key=lambda x: x[1])[1] + 1
@@ -99,7 +160,7 @@ def Triangulate(pointList):
 	s = 50 # super triangle scale factor
 	# If this scale factor is not properly larger than the infinity scale factor,
 	# the super triangle will not encapsulate the infinity points correctly and the circumcenter
-	# for the infinity points will b
+	# for the infinity points will bug out
 
 	# Create the 3 points for our super triangle, must also encapsulate infinity points
 	st_bl = (  TR[0]*(-s), TR[1]*(-s)  )
@@ -134,35 +195,60 @@ def Triangulate(pointList):
 				
 
 
-points = [(random.randrange(0,100), random.randrange(0, 100)) for x in range(50)]
+points = [(random.randrange(0,200, 3), random.randrange(0, 200, 3)) for x in range(50)]
 
-
-
-
+points = [
+	(1, 1),
+	(3, 1),
+	(4, 2),
+	(3, 5),
+	(2, 4),
+	(1, 3)
+]
 
 triangles = Triangulate(points)
+
+
+def Voronoiify(pointList):
+	tris = Triangulate(pointList)
+	voronois = []
+
+	for point in pointList:
+		l = []
+		for tri in tris:
+			if point in tri.points:
+				l.append(tri)
+		l.append(point) # to determine what the shared point is
+		return l
+		break
+
+
+v = VoronoiCell(Voronoiify(points))
 
 plt.gcf().gca().axis("equal")
 plt.axis([0, 100, 0, 100])
 
+def plotA():
+	for t in triangles:
 
-for t in triangles:
-			
-	plt.plot(t.p1[0], t.p1[1], "ro") # The three points of the triangle
-	plt.plot(t.p2[0], t.p2[1], "ro")
-	plt.plot(t.p3[0], t.p3[1], "ro")
-	plt.plot(t.cc[0], t.cc[1], "bo")
-	"""
-	circle = plt.Circle( (t.cc[0], t.cc[1]), t.ccr, color='b', fill=False)
-	poly = plt.Polygon( [[t.p1[0], t.p1[1]], [t.p2[0], t.p2[1]], [t.p3[0], t.p3[1]]], closed=True, ec="r")
-	
-	#plt.gcf().gca().add_artist(circle)
-	plt.gcf().gca().add_artist(poly)
-"""
+		plt.plot(t.p1[0], t.p1[1], "ro") # The three points of the triangle
+		plt.plot(t.p2[0], t.p2[1], "ro")
+		plt.plot(t.p3[0], t.p3[1], "ro")
+		plt.plot(t.cc[0], t.cc[1], "bo")
 
-# Voronoi
-for t in triangles:
-	nbs = findNeighbors(t, triangles)
-	for nb in nbs:
-		plt.plot([t.cc[0], nb.cc[0]], [t.cc[1], nb.cc[1]], "k-")
-plt.show()
+		circle = plt.Circle( (t.cc[0], t.cc[1]), t.ccr, color='b', fill=False)
+		poly = plt.Polygon( [[t.p1[0], t.p1[1]], [t.p2[0], t.p2[1]], [t.p3[0], t.p3[1]]], closed=True, ec="r", fill=False)
+
+		#plt.gcf().gca().add_artist(circle)
+		#plt.gcf().gca().add_artist(poly)
+
+
+	# Voronoi
+	for t in triangles:
+		nbs = findNeighbors(t, triangles)
+		for nb in nbs:
+			plt.plot([t.cc[0], nb.cc[0]], [t.cc[1], nb.cc[1]], "k-")
+	plt.show()
+
+plotA()
+
